dfffdfda7623639ee20ccc839abb5b7ad29a83e8
crystal19850615 <wubo-cc@163.com>
2018-04-12 14:55:30 +0800

    Signed-off-by: crystal19850615 <wubo-cc@163.com>

---
+++ crc.cpp
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include "crc.h"
+
+uint16_t crc16(uint8_t *buffer, uint16_t buffer_length)
+{
+    uint8_t crc_hi = 0xFF; /* high CRC byte initialized */
+    uint8_t crc_lo = 0xFF; /* low CRC byte initialized */
+    unsigned int i; /* will index into CRC lookup */
+
+    /* pass through message buffer */
+    while (buffer_length--) {
+        i = crc_hi ^ *buffer++; /* calculate the CRC  */
+        crc_hi = crc_lo ^ table_crc_hi[i];
+        crc_lo = table_crc_lo[i];
+    }
+
+    return (crc_hi << 8 | crc_lo);
+}
